689563fb6e179ed495d801ed06caa3569c918c8c
shenbinghui <657699272@qq.com>
2020-11-08 12:54:42 +0800

    新建工程

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import App from './App.vue'
+import '@shenbinghui/vue-ui-components-shenbinghui-test'
+
+Vue.config.productionTip = false
+
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
